e8576903da7d058a42501b61f14b9c6869c566f1
Nebiha <shegolenur@hotmail.com>
2023-09-08 21:36:38 +0000

    portfolio-project

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+-- prepares a MySQL server for the project
+
+CREATE DATABASE IF NOT EXISTS piyasa_dev_db;
+CREATE USER IF NOT EXISTS 'piyasa_dev'@'localhost' IDENTIFIED BY 'piyasa_dev_pwd';
+GRANT ALL PRIVILEGES ON `piyasa_dev_db`.* TO 'piyasa_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'piyasa_dev'@'localhost';
+FLUSH PRIVILEGES;
